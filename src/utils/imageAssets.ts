83606e1f51732ec2f58f7a9e95a8fdfc5a5e0617
kristensauer <sauerk1@newpaltz.edu>
2026-05-05 21:33:20 +0000

    let's try these images again...

---
+++ src/utils/imageAssets.ts
@@ -0,0 +1,27 @@
+import postImage1 from '@assets/images/posts/post-image-1.png'
+import postImage2 from '@assets/images/posts/post-image-2.png'
+import postImage3 from '@assets/images/posts/post-image-3.png'
+import postImage4 from '@assets/images/posts/post-image-4.png'
+import postImage5 from '@assets/images/posts/post-image-5.png'
+import postImage6 from '@assets/images/posts/post-image-6.png'
+import projectImage1 from '@assets/images/projects/project-image-1.png'
+import projectImage2 from '@assets/images/projects/project-image-2.png'
+import projectImage3 from '@assets/images/projects/project-image-3.png'
+import projectImage4 from '@assets/images/projects/project-image-4.png'
+import projectImage5 from '@assets/images/projects/project-image-5.png'
+import projectImage6 from '@assets/images/projects/project-image-6.png'
+
+export {
+  postImage1,
+  postImage2,
+  postImage3,
+  postImage4,
+  postImage5,
+  postImage6,
+  projectImage1,
+  projectImage2,
+  projectImage3,
+  projectImage4,
+  projectImage5,
+  projectImage6,
+}
